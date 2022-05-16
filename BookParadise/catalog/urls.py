4c4a583610cc1a32b0cb747756f82b1cbffa3781
Alen Labovich <amlam2@yandex.by>
2022-05-16 16:45:13 +0300

    fourth commit

--- BookParadise/catalog/urls.py
+++ BookParadise/catalog/urls.py
@@ -1,12 +1,31 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from . import views
-
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('authors_add/', views.authors_add, name='authors_add'),
+    path('edit1/<int:id>/', views.edit1, name='edit1'),
+    path('create/', views.create, name='create'),
+    path('delete/<int:id>/', views.delete, name='delete'),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
 ]
 
 
-# urlpatterns += [
-#     re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-# ]
+# Добавление URL-адреса для входа в систему
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
+
+urlpatterns += [
+    re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
+
+
+urlpatterns += [
+    re_path(r'^book/create/$', views.BookCreate.as_view(), name='book_create'),
+    re_path(r'^book/update/(?P<pk>\d+)$', views.BookUpdate.as_view(), name='book_update'),
+    re_path(r'^book/delete/(?P<pk>\d+)$', views.BookDelete.as_view(), name='book_delete'),
+]
